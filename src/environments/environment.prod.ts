12e0a8c44a5c6336c274a7dd4c373f1393230547
Oscar Antayhua Castillo <u20191e414@upc.edu.pe>
2025-07-09 16:37:23 -0500

    feat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,5 @@ export const environment = {
   production: true,
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.ERROR,
-  serverBasePath: (typeof process !== 'undefined' && process.env && process.env['serverBasePath'])
-    ? process.env['serverBasePath']
-    : 'https://gateway.thankfulflower-0d736896.brazilsouth.azurecontainerapps.io'
+  serverBasePath: 'https://gateway.thankfulflower-0d736896.brazilsouth.azurecontainerapps.io'
 };
