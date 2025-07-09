d9e3e97bb216a4fde6a2cd7b47acf2b261e7ab62
Oscar Antayhua Castillo <u20191e414@upc.edu.pe>
2025-07-09 16:33:39 -0500

    feat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,8 @@ import { NgxLoggerLevel } from 'ngx-logger';
 export const environment = {
   production: true,
   logLevel: NgxLoggerLevel.OFF,
-  serverLogLevel: NgxLoggerLevel.ERROR
+  serverLogLevel: NgxLoggerLevel.ERROR,
+  serverBasePath: (typeof process !== 'undefined' && process.env && process.env['serverBasePath'])
+    ? process.env['serverBasePath']
+    : 'https://gateway.thankfulflower-0d736896.brazilsouth.azurecontainerapps.io'
 };
